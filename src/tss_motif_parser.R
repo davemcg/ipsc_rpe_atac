27e8d1d6a5db2e9bf33a457c80bf67e8162a3400
David McGaughey <mcgaughey@gmail.com>
2018-05-30 09:40:18 -0400

    print intermediate files for debugging

--- src/tss_motif_parser.R
+++ src/tss_motif_parser.R
@@ -11,7 +11,7 @@ library(tidyverse)
 
 # load gtf to get gene name 
 gtf <- read_tsv(gtf_path, col_names = c('Transcript','Gene'))
-
+print(head(gtf))
 # load in nearest tss motif info
 sample <- str_split(str_split(file_name, '/')[[1]][2], '\\.')[[1]][1]
 input <- read_tsv(file_name, col_names = c('sequence_name', 'start', 'end', 'motif', 'fimo_pvalue', 'strand', 'tss_seq','tss_start','tss_end', 'transcript',  'blank','tss_strand','coord1','coord2','blank2','exon_num','size','exon_pos','distance'))
@@ -25,11 +25,11 @@ input <- input %>% mutate(sequence_name = as.character(sequence_name),
   mutate(sample = as.factor(sample)) %>% 
   rowwise() %>% 
   mutate(Transcript = str_split(transcript, '_')[[1]][1])
-
+print(head(input))
 both <- left_join(input, gtf) %>% 
   filter(!is.na(Gene)) %>% 
   mutate(motif_loc = paste(sequence_name, start, end, sep='_'))
-
+print(head(both))
 out <- both %>%
   # remove any TSS over 500kb away
   filter(distance < 500000) %>%
@@ -48,7 +48,9 @@ out <- both %>%
 # %>%
 #   summarise(Count=n(), Motif_Regions = paste(motif_loc, collapse=', ')) %>%
 #   arrange(-Count)
+print(head(out))
 
+print(dim(out))
 write_tsv(out, 
           path = output_name)
 
