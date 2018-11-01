65364f22f934566e19ed9b00e5eb8791cd893df6
David McGaughey <mcgaughey@gmail.com>
2018-11-01 16:04:46 -0400

    rscript for filter rule

---
+++ src/filter_match_results.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+args = commandArgs(trailingOnly=TRUE)
+
+known_file <- args[1]
+novel_file <- args[2]
+
+known <- read_tsv(known_file)
+novel <- read_tsv(novel_file)
+
+both <- bind_rows(known %>% select(HomerName, `P-value`, padj, log2FoldChange, GFP, RFP, iPSC))
+
+output <- both %>% filter(`P-value` < 1e-20, padj < 0.01, abs(log2FoldChange) > 1) %>% pull(HomerName)
+
+write(output, file = args[3])
+
