c8d5683fa3eb268907dcbccdff3732228ac4ed9c
David McGaughey <mcgaughey@gmail.com>
2018-11-05 10:15:34 -0500

    major rework to analyze by all TF

--- src/analyze_macs2_homer_merge.R
+++ src/analyze_macs2_homer_merge.R
@@ -1,29 +1,48 @@
 library(tidyverse)
-peak <- read_tsv('/Volumes/data/projects/nei/hufnagel/iPSC_RPE_ATAC_Seq/peak_full/all_common_peaks.blackListed.narrowPeak.closestTSS__interesting_homer_motif.bed.gz', col_names = F)
+peak <- read_tsv('~/Downloads/all_common_peaks.blackListed.narrowPeak.closestTSS__interesting_homer_motif.bed.gz', col_names = F)
 expression <- read_csv('~/git/ipsc_rpe_RNA-seq/data/lsTPM_by_Line_with_Diff_Exp.tsv')
 
-otx2 <- peak %>% 
-  filter(!is.na(X11)) %>% # remove NA genes where the peak didn't get a match (no genes within 500kb of a peak)
-  filter(grepl('OTX2', X16, ignore.case = T)) %>% # only retain OTX2 TFBS homer hits
-  group_by(X4, X1, X2, X3, X11) %>% # group by GFP/RFP,iPSC, the macs2 peak, and the gene
-  summarise(Num_OTX2_motifs=n()) %>%  # count total number of OTX2 motifs
-  group_by(X4, X11) %>% # group now by GFP/RFP/IPSC and gene
-  summarise(Total_OTX2_motifs = sum(Num_OTX2_motifs), Num_Peaks = n()) %>% # now add num of peaks near gene
-  select(Tissue = X4, Gene = X11, Total_OTX2_motifs, Num_Peaks)
+colnames(peak) <- c('chrom','start','stop','type','macs2_score','strand','thickStart','thickStop','RGB','Distance_to_Gene','Gene','Description','chrom_motif','start_motif','stop_motif','motif','motif_score','strand_motif')
 
+motifs <- peak$motif %>% unique()
 
+# create gene centric info on peak/motif and differential gene expression iPSC <-> RPE
+grouped_stats <- list()
 
-mitf <- peak %>% 
-  filter(!is.na(X11)) %>% # remove NA genes where the peak didn't get a match (no genes within 500kb of a peak)
-  filter(grepl('MITF', X16, ignore.case = T)) %>% # only retain OTX2 TFBS homer hits
-  group_by(X4, X1, X2, X3, X11) %>% # group by GFP/RFP,iPSC, the macs2 peak, and the gene
-  summarise(Num_TFBS_motifs=n()) %>%  # count total number of OTX2 motifs
-  group_by(X4, X11) %>% # group now by GFP/RFP/IPSC and gene
-  summarise(Total_TFBS_motifs = sum(Num_TFBS_motifs), Num_Peaks = n()) %>% # now add num of peaks near gene
-  select(Tissue = X4, Gene = X11, Total_TFBS_motifs, Num_Peaks)
+for (i in motifs){
+  out <- peak %>% 
+    filter(!is.na(Gene)) %>% # remove NA genes where the peak didn't get a match (no genes within 500kb of a peak)
+    filter(motif == i) %>% # only retain the motif TFBS homer hits
+    group_by(type, chrom, start, stop, Gene) %>% # group by GFP/RFP,iPSC, the macs2 peak, and the gene
+    summarise(Num_TFBS_motifs=n()) %>%  # count total number of motifs
+    group_by(type, Gene) %>% # group now by GFP/RFP/IPSC and gene
+    summarise(Total_TFBS_motifs = sum(Num_TFBS_motifs), Num_Peaks = n()) %>% # now add num of peaks near gene
+    select(type, Gene, Total_TFBS_motifs, Num_Peaks)
+  grouped_stats[[i]] <- out
+  print(i)
+}
 
-mitf %>% spread(Tissue, Num_Peaks) %>% arrange(-GFP)
+# further cut down to most interesting genes by differential peak activity and 
+# expression by TF
 
-mitf %>% select(-Total_TFBS_motifs) %>% spread(Tissue, Num_Peaks) %>% mutate(GFP_iPSC = GFP/iPSC) %>% arrange(-GFP_iPSC)
+TF_gene_stats_list <- list()
 
-mitf %>% select(-Total_TFBS_motifs) %>% spread(Tissue, Num_Peaks) %>% mutate(GFP_iPSC = GFP/iPSC) %>% arrange(-GFP_iPSC) %>% left_join(expression %>% select(-Line, -lsTPM, -`log2(lsTPM)`, -Rank) %>% unique())
+for (i in motifs){
+  
+  # ID differentially peak activity around genes for a TF
+  out <- grouped_stats[[i]] %>% select(-Total_TFBS_motifs) %>% 
+    spread(type, Num_Peaks) %>% 
+    mutate(ratio = GFP/iPSC, delta = GFP - iPSC) %>% 
+    arrange(-delta) %>% 
+    left_join(expression %>% 
+                select(Gene, log2FoldChange__RPE_vs_iPSC, padj__RPE_vs_iPSC) %>% 
+                unique()) %>% 
+    filter(!is.na(padj__RPE_vs_iPSC), 
+           abs(log2FoldChange__RPE_vs_iPSC) > 1, 
+           padj__RPE_vs_iPSC < 0.01) %>% 
+    filter(delta > 4, ratio > 1.5)
+  TF_gene_stats_list[[i]] <- out
+  print(i)
+}
+
+TF_gene_stats <- bind_rows(TF_gene_stats_list, .id = 'motif')
