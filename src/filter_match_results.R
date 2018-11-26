f34aeb8b39837a2567710fdda8f9298f970de1d1
David McGaughey <mcgaughey@gmail.com>
2018-11-26 11:01:36 -0500

    dont filter tf on log2FC

--- src/filter_match_results.R
+++ src/filter_match_results.R
@@ -9,7 +9,8 @@ novel <- read_tsv(novel_file)
 
 both <- bind_rows(known %>% select(HomerName, `P-value`, padj, log2FoldChange, GFP, RFP, iPSC))
 
-output <- both %>% filter(`P-value` < 1e-20, padj < 0.01, abs(log2FoldChange) > 1) %>% pull(HomerName)
+#output <- both %>% filter(`P-value` < 1e-20, padj < 0.01, abs(log2FoldChange) > 1) %>% pull(HomerName)
+output <- both %>% filter(GFP > 1) %>% pull(HomerName)
 
 write(output, file = args[3])
 
