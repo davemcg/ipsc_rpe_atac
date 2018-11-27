60c7be2eebdaccb678fb774f5f65e5f7901ea6e5
David McGaughey <mcgaughey@gmail.com>
2018-11-27 13:58:39 -0500

    remove TFBS with lower q values

--- src/filter_match_results.R
+++ src/filter_match_results.R
@@ -7,10 +7,10 @@ novel_file <- args[2]
 known <- read_tsv(known_file)
 novel <- read_tsv(novel_file)
 
-both <- bind_rows(known %>% select(HomerName, `P-value`, padj, log2FoldChange, GFP, RFP, iPSC))
+both <- bind_rows(known %>% select(HomerName, `q-value (Benjamini)`,  `P-value`, padj, log2FoldChange, GFP, RFP, iPSC))
 
 #output <- both %>% filter(`P-value` < 1e-20, padj < 0.01, abs(log2FoldChange) > 1) %>% pull(HomerName)
-output <- both %>% filter(GFP > 1) %>% pull(HomerName)
+output <- both %>% filter(GFP > 1, `q-value (Benjamini)` < 0.001) %>% pull(HomerName)
 
 write(output, file = args[3])
 
